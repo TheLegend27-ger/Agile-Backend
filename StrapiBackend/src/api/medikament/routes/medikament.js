64994afb16f8e3445e4cd069e22ce2010f5b18d9
Tim Jonzek <55186709+TheLegend27-ger@users.noreply.github.com>
2022-09-30 14:43:35 +0200

    CollectionType und GET Token erstellt

---
+++ StrapiBackend/src/api/medikament/routes/medikament.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * medikament router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::medikament.medikament');
---
+++ StrapiBackend/src/api/medikament/services/medikament.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * medikament service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::medikament.medikament');
