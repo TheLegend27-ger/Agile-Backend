c75af26643b8e9a266eb3fa4590d7930510fa06d
Tim Jonzek <55186709+TheLegend27-ger@users.noreply.github.com>
2022-11-25 15:47:25 +0100

    W

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -1,12 +1,41 @@
 import datetime
 import logging
-
 import azure.functions as func
-
+import requests
+import json
+import pymongo 
+from bs4 import BeautifulSoup
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
+
+
+
+    client = pymongo.MongoClient("mongodb+srv://Cluster2User:gPId3Gm4a0I9icN5@agile2cluster.bxvcw4l.mongodb.net/?retryWrites=true&w=majority",tls=True, tlsAllowInvalidCertificates=True)
+    mydb = client["Test"]
+    mycol = mydb["CollectionTest"]
+    mycol.drop()
+
+
+    response = requests.get('https://www.hpra.ie/img/uploaded/swedocuments/latestVMlist.xml')
+    print("download done")
+    data = response.content
+
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(data, 'xml')
+
+    # filter out all productnames into list of dictionaries
+    parent = soup.find('Products')
+    products = []
+    for n, product in enumerate(parent.find_all('Product')):
+        if product.find('Species').text.strip() == 'Horses':
+            dict = {"name" : product.find('ProductName').text.strip()}
+            products.append(dict)
+
+    json_data = json.dumps(products, indent = 2)
+    print(mycol.insert_many(products))
+
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
