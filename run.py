8fa277661cd8fa8b64e5ea7a34ad9538c03583fb
Tim <55186709+TheLegend27-ger@users.noreply.github.com>
2022-12-15 12:06:58 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#Backup file for The Python WebSraper please look at the code within the TimerTrigger for the full Code
 import requests
 import json
 import pymongo 
@@ -5,14 +6,20 @@ from bs4 import BeautifulSoup
 import lxml
 
 
-
+#Create connection String
 client = pymongo.MongoClient("mongodb+srv://Cluster2User:gPId3Gm4a0I9icN5@agile2cluster.bxvcw4l.mongodb.net/?retryWrites=true&w=majority",tls=True, tlsAllowInvalidCertificates=True)
+
+#Create DataBase if not already there
 db = client.test
 mydb = client["Test"]
+
+#Create collection
 mycol = mydb["CollectionTest"]
+
+#Delete all entries in Collection
 mycol.drop()
 
-
+#Get HTML/XML page from web
 response = requests.get('https://www.hpra.ie/img/uploaded/swedocuments/latestVMlist.xml')
 print("download done")
 data = response.content
